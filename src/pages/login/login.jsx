b96c13e046b3d2e2a8d4abfd55de3b127a5a8128
Adrian <druids8012@gmail.com>
2020-07-23 23:00:58 +0800

    07-23

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './css/login.less'
+import logo from "./imgs/logo.png"
+import { Form, Input, Button} from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+class Login extends Component {
+  handleSubmit =() =>{
+    alert('发送请求');
+  };
+
+  onFinish = (values) => {
+      console.log('Finish:', values);
+  };
+  
+    render() {
+        return (
+          
+            <div className = "login">
+                <header>
+                  <img src={logo} alt=""/>                  
+                  <h1>商品管理系统</h1>
+                </header>
+
+                <section>
+                  <h1>用户登录</h1>
+                        <Form
+                              name="normal_login"
+                              className="login-form"
+                              initialValues={{
+                                remember: true,
+                              }}
+                              onFinish={this.onFinish}
+                            >
+                              <Form.Item
+                                name="username"
+                                rules={[
+                                  {
+                                    required: true,
+                                    message: 'Please input your Username!',
+                                  },
+                                  {max:12,message:"用户名必须小于等于12位"},
+                                  {min:4, message:"用户名必须大于等于4位"},
+                                  {pattern:/^\w+$/, message:"用户名必须是字母,数字,下划线"},
+                                ]}
+                              >
+                                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                              </Form.Item>
+                              <Form.Item
+                                name="password"
+                                
+                                rules={[
+                                  {
+                                    required: true,
+                                    message: 'Please input your Password!',
+                                  },
+                                ]}
+                              >
+                                <Input
+                                  prefix={<LockOutlined className="site-form-item-icon" />}
+                                  type="password"
+                                  placeholder="Password"
+                                />
+                              </Form.Item>
+
+                              <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                  Log in
+                                </Button>
+                              </Form.Item>
+                            </Form>
+                </section>
+            </div>
+        );
+    }
+}
+
+
+
+export default Login;
