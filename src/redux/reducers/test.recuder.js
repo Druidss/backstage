e6400b5fcb4f785302fbfc688346bd839461d705
Adrian <druids8012@gmail.com>
2020-07-24 23:15:23 +0800

    redux

---
+++ src/redux/reducers/test.recuder.js
@@ -0,0 +1,13 @@
+let initState = 'hello'
+function test (preState = initState, action){
+    const {type,data} = action;
+    let newState
+    switch (type) {
+        case action:
+            
+            break;
+    
+        default:
+            break;
+    }
+} 
